ba44ad58db1d1566afe1d947206a315c45c9ab16
Nikita <on11shchenko@gmail.com>
2022-05-17 14:07:35 +0300

    update

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,6 +1,7 @@
 <template>
   <router-link class="event-link" 
-              :to="{ name: 'EventDetails', params: { id: event.id } }">
+    :to="{ name: 'EventDetails', params: { id: event.id } }"
+  >
     <div class="event-card">
       <span>@{{ event.time }} on {{ event.date }}</span>
       <h4>{{ event.title }}</h4>
